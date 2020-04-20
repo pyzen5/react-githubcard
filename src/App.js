a1087ecd17db3eca40fd045ac31e96b98e334be6
pyzen5 <pyzen55@gmail.com>
2020-04-20 12:24:22 +0530

    real api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 const testData = [
   {name: 'Person 1', avatar_url: 'https://via.placeholder.com/100', company: 'Facebook'},
@@ -30,7 +31,7 @@ class Card extends Component {
 
     return (
       <div style={{display: 'flex', margin: '10px', padding: '10px'}}>
-        <img src={profile.avatar_url}></img>
+        <img src={profile.avatar_url} width="150px" height="150px"></img>
         <div style={stylle1}>
           <h3>Name: {profile.name} </h3>
           <p>Profile: {profile.company} </p>
@@ -44,10 +45,13 @@ class Card extends Component {
 class Form extends Component {
   // userNameInput = React.createRef();
   state = { userName: ''}
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`)
+    console.log(resp.data)
     // console.log(this.userNameInput.current.value)
     console.log(this.state.userName)
+    this.props.addUser(resp.data)
   }
 
   render() {
@@ -70,14 +74,21 @@ class App extends Component {
   // }
 
   state = {
-    profiles: testData
+    profiles: []
+  }
+
+  addNewUser = (profileData) => {
+    console.log('App', profileData);
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }))
   }
 
   render() {
     return (
       <div>
         <h1>Github Profiles</h1>
-        <Form />
+        <Form addUser={this.addNewUser} />
         <CardList profiles={this.state.profiles}/>
       </div>
     )
